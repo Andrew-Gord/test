134fbdddc2e96654b3dd135bd4b4376e418286c0
Andrew-Gord <Andrew.gordon24@ncf.edu>
2023-10-04 10:24:59 -0400

    first update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+import { CalendarComponent } from './calendar/calendar.component';
+import { Tab1Component } from './tab1/tab1.page';
+import { Tab2Component } from './tab2/tab2.page';
+import { Tab3Component } from './tab3/tab3.page';
+
+
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  {path:'',component: Tab1Component},
+  {path:"tab2",component:Tab2Component},
+  {path:"tab3",component:Tab3Component},
+  {path:"calendar",component:CalendarComponent},
+
 ];
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
